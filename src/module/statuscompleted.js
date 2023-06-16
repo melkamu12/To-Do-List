17f87c42fa9519b7629ee8cb3bbfd301ed3957cc
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-06-16 13:24:33 +0300

    Update Refresh icon

---
+++ src/module/statuscompleted.js
@@ -0,0 +1,9 @@
+import { ToDoList, storeTasksToLocalStorage } from "./todo.js";
+function updateStatus(index, isChecked) {
+  const itemToChange = ToDoList.find((item) => item.index === index);
+  if (itemToChange) {
+    itemToChange.completed = isChecked;
+    storeTasksToLocalStorage();
+  }
+}
+export default updateStatus;
